1ac3464b3dde6e347e53d79c672925f93cf88e5e
Robin Parker <Mdaffan0502@gmail.com>
2025-04-30 19:14:45 +0530

    Isolated Library

--- Library_Management_System/Main.java
+++ Library_Management_System/Main.java
@@ -1,6 +1,5 @@
 package Library_Management_System;
 
-import java.util.HashMap;
 
 public class Main {
 
@@ -15,36 +14,3 @@ public class Main {
     }
 }
 
-class Library {
-
-    private final HashMap<Integer, Book> books = new HashMap<>();
-    private int Id = 100;
-
-    public void initializeBooks() {
-        Book eBook = new EBook("American Psycho", "Bret Easton Ellis", 500);
-        Book eBook2 = new EBook("The Shining", "Stephen King", 800);
-
-        Book printedBook = new PrintedBooks("Crimes and Punishment", "Fyodor Dostoevsky", 3);
-        Book printedBook2 = new PrintedBooks("The Stranger", "Albert Camus", 9);
-
-        addBook(eBook);
-        addBook(eBook2);
-
-        addBook(printedBook);
-        addBook(printedBook2);
-    }
-
-    private void addBook(Book book) {
-        books.put(Id, book);
-        Id++;
-    }
-
-    public void displayBook() {
-
-        for (Book book : books.values()) {
-            book.bookInfo(); 
-            System.out.println("---------------------------");
-        }
-    }
-
-}
